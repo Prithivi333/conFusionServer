f3f8a3bc27a13494c4cf47b38c7b13647e005c0d
Prithivi <prithivi8teen@gmail.com>
2022-01-02 23:24:47 +0530

    Express sessions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,43 +32,36 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-14523-96587'));
+//app.use(cookieParser('12345-67890-14523-96587'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-21547-87625',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      var err = new Error('Unauthorized user');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-    var auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-    if (auth[0] === 'admin' && auth[1] === 'password') {
-      res.cookie('user','admin',{signed:true});
+  if (!req.session.user) {
+    var err = new Error('Unauthorized user');
+    err.status = 401;
+    return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
       var err = new Error('Unauthorized user');
 
       res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-  }
-  else{
-    if(req.signedCookies.user==='admin'){
-      next();
-    }
-    else{
-      var err = new Error('Unauthorized user');
-
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -76,8 +71,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
